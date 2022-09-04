35143375b43d48be595f7c95148fa53bd2b9d9ea
taniavaldivia <tania.valdiviag@gmail.com>
2022-09-04 00:05:44 -0500

    deploy

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.json())
 app.all('*', function(req, res, next) {
   var origin = req.get('origin') 
   res.header('Access-Control-Allow-Origin', origin)
-  res.header("Access-Control-Allow-Headers", "X-Requested-With")
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With')
   res.header('Access-Control-Allow-Headers', 'Content-Type')
   next()
 })
